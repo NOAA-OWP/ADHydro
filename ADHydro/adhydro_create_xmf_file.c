d400251a967570734a481fdca5ef52c7dcc7e8d9
Leticia Pureza <lpureza@chow.uwyo.edu>
2014-06-24 14:40:05 -0600

    Warning messages taken care.
    Former-commit-id: a8147a8ca32675606d85863ad1f1d4d6de51851c

--- ADHydro/adhydro_create_xmf_file.c
+++ ADHydro/adhydro_create_xmf_file.c
@@ -8,13 +8,12 @@
 // Xdmf files.
 // The purpose of this program is to generate the Xdmf files from the .nc
 // files. All the output information are in the .nc files. 
-// 06 - 09 - 2014
-// Leticia Pureza 
+
 int main(int argc, char*argv[])
 {
  char *namestring;				// string to save the file names and directories
  int err_status;				// error variable
- int n;						// counter
+ int nn;					// counter
  int ncid_stt;					// id of the state.nc file			
  int numgrps_stt;				// # of groups in the state.nc file
  int *grpid_stt = NULL;				// id of each group of the state.nc file
@@ -73,26 +72,26 @@ fprintf(output,"  <Domain>\n");
 fprintf(output,"    <Grid CollectionType=\"Temporal\" GridType=\"Collection\" Name=\"Mesh\">\n\n"); 
  
  
- for (n = 0; n < numgrps_stt; n++)
+ for (nn = 0; nn < numgrps_stt; nn++)
  {
  	// time step
  	
  	// getting the group name from the group id for the state.nc file
- 	err_status = nc_inq_grpname(grpid_stt[n], grp_stt_name);
+ 	err_status = nc_inq_grpname(grpid_stt[nn], grp_stt_name);
 	if (err_status != NC_NOERR)
  	{
     	    printf("Problem getting group name from ../state.nc\n");
       	    exit(0);
  	}
 
- 	err_status = nc_get_att_double (grpid_stt[n], NC_GLOBAL, "time", &time);
+ 	err_status = nc_get_att_double (grpid_stt[nn], NC_GLOBAL, "time", &time);
  	if (err_status != NC_NOERR)
  	{
  	     printf("Problem getting attributes of ../state.nc\n");
  	     exit(0);
  	}
 	
- 	err_status = nc_get_att_int (grpid_stt[n], NC_GLOBAL, "geometryGroup", &grp_geo);
+ 	err_status = nc_get_att_int (grpid_stt[nn], NC_GLOBAL, "geometryGroup", &grp_geo);
  	
  	if (err_status != NC_NOERR)
  	{
@@ -100,14 +99,14 @@ fprintf(output,"    <Grid CollectionType=\"Temporal\" GridType=\"Collection\" Na
       	     exit(0);
  	}
 	
-	err_status = nc_get_att_int (grpid_stt[n], NC_GLOBAL, "parameterGroup", &grp_par);
+	err_status = nc_get_att_int (grpid_stt[nn], NC_GLOBAL, "parameterGroup", &grp_par);
  	if (err_status != NC_NOERR)
  	{
      	    printf("Problem getting attributes of state.nc\n");
 	    exit(0);
  	}
 
- 	err_status = nc_inq_dimid (grpid_stt[ n ],"numberOfMeshElements" , &neleID);
+ 	err_status = nc_inq_dimid (grpid_stt[ nn ],"numberOfMeshElements" , &neleID);
  	if (err_status != NC_NOERR)
  	{
       	    printf("Problem getting dimmesion id from ../state.nc\n");
@@ -115,14 +114,14 @@ fprintf(output,"    <Grid CollectionType=\"Temporal\" GridType=\"Collection\" Na
  	}
  	
  	// # elements  
- 	err_status = nc_inq_dimlen  (grpid_stt[ n ], neleID, &nele);
+ 	err_status = nc_inq_dimlen  (grpid_stt[ nn ], neleID, &nele);
   	if (err_status != NC_NOERR)
  	{
 	    printf("Problem getting dimmesion from ../state.nc\n");
       	    exit(0);
  	}
   	
-  	err_status = nc_inq_dimid (grpid_stt[ n ],"numberOfMeshNodes" , &nnodeID);
+  	err_status = nc_inq_dimid (grpid_stt[ nn ],"numberOfMeshNodes" , &nnodeID);
   	if (err_status != NC_NOERR)
  	{
       	   printf("Problem getting dimmesion id from ../state.nc\n");
@@ -130,7 +129,7 @@ fprintf(output,"    <Grid CollectionType=\"Temporal\" GridType=\"Collection\" Na
  	}
  	
  	// dimension size  
- 	err_status = nc_inq_dimlen  (grpid_stt[ n ], nnodeID, &nnode);
+ 	err_status = nc_inq_dimlen  (grpid_stt[ nn ], nnodeID, &nnode);
   	if (err_status != NC_NOERR)
  	{
     	    printf("Problem getting dimmesion from ../state.nc\n");
@@ -140,78 +139,78 @@ fprintf(output,"    <Grid CollectionType=\"Temporal\" GridType=\"Collection\" Na
 // for all groups
 fprintf(output,"      <Grid GridType=\"Uniform\">\n");
 fprintf(output,"        <Time Value=\"%f\"/>\n",time);
-fprintf(output,"        <Topology NumberOfElements=\"%i\" Type=\"Triangle\">\n",nele);
-fprintf(output,"          <DataItem DataType=\"Int\" Dimensions=\"%i 3\" Format=\"HDF\">geometry.nc:/%i/meshElementNodeIndices</DataItem>\n",nele,grp_geo);
+fprintf(output,"        <Topology NumberOfElements=\"%zu\" Type=\"Triangle\">\n",nele);
+fprintf(output,"          <DataItem DataType=\"Int\" Dimensions=\"%zu 3\" Format=\"HDF\">geometry.nc:/%i/meshElementNodeIndices</DataItem>\n",nele,grp_geo);
 fprintf(output,"        </Topology>\n");
 fprintf(output,"        <Geometry Type=\"XYZ\">\n");
-fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%i 3\" Format=\"HDF\" Precision=\"8\">geometry.nc:/%i/meshNodeXYZSurfaceCoordinates</DataItem>\n",nnode,grp_geo);
+fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%zu 3\" Format=\"HDF\" Precision=\"8\">geometry.nc:/%i/meshNodeXYZSurfaceCoordinates</DataItem>\n",nnode,grp_geo);
 fprintf(output,"        </Geometry>\n");
 fprintf(output,"        <Attribute AttributeType=\"Scalar\" Center=\"Node\" Name=\"NodeZBedrock\">\n");
-fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%i\" Format=\"HDF\" Precision=\" 8\">geometry.nc:/%i/meshNodeZBedrockCoordinates</DataItem>\n",nnode,grp_geo);
+fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%zu\" Format=\"HDF\" Precision=\" 8\">geometry.nc:/%i/meshNodeZBedrockCoordinates</DataItem>\n",nnode,grp_geo);
 fprintf(output,"        </Attribute>\n");
 fprintf(output,"        <Attribute AttributeType=\"Scalar\" Center=\"Edge\" Name=\"EdgeLength\">\n");
-fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%i 3\" Format=\"HDF\" Precision=\"8\">geometry.nc:/%i/meshEdgeLength</DataItem>\n",nele,grp_geo);
+fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%zu 3\" Format=\"HDF\" Precision=\"8\">geometry.nc:/%i/meshEdgeLength</DataItem>\n",nele,grp_geo);
 fprintf(output,"        </Attribute>\n");
 fprintf(output,"        <Attribute AttributeType=\"Scalar\" Center=\"Edge\" Name=\"EdgeNormalX\">\n");
-fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%i 3\" Format=\"HDF\" Precision=\"8\">geometry.nc:/%i/meshEdgeNormalX</DataItem>\n",nele,grp_geo);
+fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%zu 3\" Format=\"HDF\" Precision=\"8\">geometry.nc:/%i/meshEdgeNormalX</DataItem>\n",nele,grp_geo);
 fprintf(output,"        </Attribute>\n");
 fprintf(output,"        <Attribute AttributeType=\"Scalar\" Center=\"Edge\" Name=\"EdgeNormalY\">\n");
-fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%i\" Format=\"HDF\" Precision=\"8\">geometry.nc:/%i/meshEdgeNormalY</DataItem>\n",nele,grp_geo);
+fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%zu\" Format=\"HDF\" Precision=\"8\">geometry.nc:/%i/meshEdgeNormalY</DataItem>\n",nele,grp_geo);
 fprintf(output,"        </Attribute>\n");
 fprintf(output,"        <Attribute AttributeType=\"Scalar\" Center=\"Cell\" Name=\"ElementX\">\n");
-fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%i\" Format=\"HDF\" Precision=\"8\">geometry.nc:/%i/meshElementX</DataItem>\n",nele,grp_geo);
+fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%zu\" Format=\"HDF\" Precision=\"8\">geometry.nc:/%i/meshElementX</DataItem>\n",nele,grp_geo);
 fprintf(output,"        </Attribute>\n");
 fprintf(output,"        <Attribute AttributeType=\"Scalar\" Center=\"Cell\" Name=\"ElementY\">\n");
 fprintf(output,"          \n");
-fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%i\" Format=\"HDF\" Precision=\"8\">geometry.nc:/%i/meshElementY</DataItem>\n",nele,grp_geo);
+fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%zu\" Format=\"HDF\" Precision=\"8\">geometry.nc:/%i/meshElementY</DataItem>\n",nele,grp_geo);
 fprintf(output,"        </Attribute>\n");
 fprintf(output,"        <Attribute AttributeType=\"Scalar\" Center=\"Cell\" Name=\"ElementZSurface\">\n");
-fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%i\" Format=\"HDF\" Precision=\"8\">geometry.nc:/%i/meshElementZSurface</DataItem>\n",nele,grp_geo);
+fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%zu\" Format=\"HDF\" Precision=\"8\">geometry.nc:/%i/meshElementZSurface</DataItem>\n",nele,grp_geo);
 fprintf(output,"        </Attribute>\n");
 fprintf(output,"        <Attribute AttributeType=\"Scalar\" Center=\"Cell\" Name=\"ElementZBedrock\">\n");
-fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%i\" Format=\"HDF\" Precision=\"8\">geometry.nc:/%i/meshElementZBedrock</DataItem>\n",nele,grp_geo);
+fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%zu\" Format=\"HDF\" Precision=\"8\">geometry.nc:/%i/meshElementZBedrock</DataItem>\n",nele,grp_geo);
 fprintf(output,"        </Attribute>\n");
 fprintf(output,"        <Attribute AttributeType=\"Scalar\" Center=\"Cell\" Name=\"ElementArea\">\n");  
-fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%i\" Format=\"HDF\" Precision=\"8\">geometry.nc:/%i/meshElementArea</DataItem>\n",nele,grp_geo);
+fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%zu\" Format=\"HDF\" Precision=\"8\">geometry.nc:/%i/meshElementArea</DataItem>\n",nele,grp_geo);
 fprintf(output,"        </Attribute>\n"); 
 fprintf(output,"        <Attribute AttributeType=\"Scalar\" Center=\"Cell\" Name=\"ElementCatchment\">\n");
-fprintf(output,"          <DataItem DataType=\"Int\" Dimensions=\"%i\" Format=\"HDF\">parameter.nc:/%i/meshElementCatchment</DataItem>\n",nele,grp_par); 
+fprintf(output,"          <DataItem DataType=\"Int\" Dimensions=\"%zu\" Format=\"HDF\">parameter.nc:/%i/meshElementCatchment</DataItem>\n",nele,grp_par); 
 fprintf(output,"        </Attribute>\n");
 fprintf(output,"        <Attribute AttributeType=\"Scalar\" Center=\"Cell\" Name=\"ElementConductivity\">\n"); 
-fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%i\" Format=\"HDF\" Precision=\"8\">parameter.nc:/%i/meshElementConductivity</DataItem>\n",nele,grp_par);
+fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%zu\" Format=\"HDF\" Precision=\"8\">parameter.nc:/%i/meshElementConductivity</DataItem>\n",nele,grp_par);
 fprintf(output,"        </Attribute>\n"); 
 fprintf(output,"        <Attribute AttributeType=\"Scalar\" Center=\"Cell\" Name=\"ElementPorosity\">\n");
-fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%i\" Format=\"HDF\" Precision=\"8\">parameter.nc:/%i/meshElementPorosity</DataItem>\n",nele,grp_par); 
+fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%zu\" Format=\"HDF\" Precision=\"8\">parameter.nc:/%i/meshElementPorosity</DataItem>\n",nele,grp_par); 
 fprintf(output,"        </Attribute>\n");
 fprintf(output,"        <Attribute AttributeType=\"Scalar\" Center=\"Cell\" Name=\"ElementManningsN\">\n"); 
-fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%i\" Format=\"HDF\" Precision=\"8\">parameter.nc:/%i/meshElementManningsN</DataItem>\n",nele,grp_par);			
+fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%zu\" Format=\"HDF\" Precision=\"8\">parameter.nc:/%i/meshElementManningsN</DataItem>\n",nele,grp_par);			
 fprintf(output,"        </Attribute>\n"); 
 fprintf(output,"        <Attribute AttributeType=\"Scalar\" Center=\"Cell\" Name=\"ElementSurfacewaterDepth\">\n");
-fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%i\" Format=\"HDF\" Precision=\"8\">state.nc:/%s/meshElementSurfacewaterDepth</DataItem>\n",nele,grp_stt_name); 
+fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%zu\" Format=\"HDF\" Precision=\"8\">state.nc:/%s/meshElementSurfacewaterDepth</DataItem>\n",nele,grp_stt_name); 
 fprintf(output,"        </Attribute>\n");
 fprintf(output,"        <Attribute AttributeType=\"Scalar\" Center=\"Cell\" Name=\"ElementSurfacewaterError\">\n");
-fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%i\" Format=\"HDF\" Precision=\"8\">state.nc:/%s/meshElementSurfacewaterError</DataItem>\n",nele,grp_stt_name);
+fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%zu\" Format=\"HDF\" Precision=\"8\">state.nc:/%s/meshElementSurfacewaterError</DataItem>\n",nele,grp_stt_name);
 fprintf(output,"        </Attribute>\n"); 
 fprintf(output,"        <Attribute AttributeType=\"Scalar\" Center=\"Cell\" Name=\"ElementGroundwaterHead\">\n");
-fprintf(output,"           <DataItem DataType=\"Float\" Dimensions=\"%i\" Format=\"HDF\" Precision=\"8\">state.nc:/%s/meshElementGroundwaterHead</DataItem>\n",nele,grp_stt_name);
+fprintf(output,"           <DataItem DataType=\"Float\" Dimensions=\"%zu\" Format=\"HDF\" Precision=\"8\">state.nc:/%s/meshElementGroundwaterHead</DataItem>\n",nele,grp_stt_name);
 fprintf(output,"        </Attribute>\n");
 fprintf(output,"        <Attribute AttributeType=\"Scalar\" Center=\"Cell\" Name=\"ElementGroundwaterError\">\n"); 
-fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%i\" Format=\"HDF\" Precision=\"8\">state.nc:/%s/meshElementGroundwaterError</DataItem>\n",nele,grp_stt_name);
+fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%zu\" Format=\"HDF\" Precision=\"8\">state.nc:/%s/meshElementGroundwaterError</DataItem>\n",nele,grp_stt_name);
 fprintf(output,"        </Attribute>\n");
 fprintf(output,"        <Attribute AttributeType=\"Scalar\" Center=\"Cell\" Name=\"ElementGroundwaterRecharge\">\n"); 
-fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%i\" Format=\"HDF\" Precision=\"8\">state.nc:/%s/meshElementGroundwaterRecharge</DataItem>\n",nele,grp_stt_name); 
+fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%zu\" Format=\"HDF\" Precision=\"8\">state.nc:/%s/meshElementGroundwaterRecharge</DataItem>\n",nele,grp_stt_name); 
 fprintf(output,"        </Attribute>\n");
 fprintf(output,"        <Attribute AttributeType=\"Scalar\" Center=\"Edge\" Name=\"EdgeSurfacewaterFlowRate\">\n"); 
-fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%i 3\" Format=\"HDF\" Precision=\"8\">state.nc:/%s/meshEdgeSurfacewaterFlowRate</DataItem>\n",nele,grp_stt_name);
+fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%zu 3\" Format=\"HDF\" Precision=\"8\">state.nc:/%s/meshEdgeSurfacewaterFlowRate</DataItem>\n",nele,grp_stt_name);
 fprintf(output,"        </Attribute>\n"); 
 fprintf(output,"        <Attribute AttributeType=\"Scalar\" Center=\"Edge\" Name=\"EdgeSurfacewaterCumulativeFlow\">\n");
-fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%i 3\" Format=\"HDF\" Precision=\"8\">state.nc:/%s/meshEdgeSurfacewaterCumulativeFlow</DataItem>\n",nele,grp_stt_name); 
+fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%zu 3\" Format=\"HDF\" Precision=\"8\">state.nc:/%s/meshEdgeSurfacewaterCumulativeFlow</DataItem>\n",nele,grp_stt_name); 
 fprintf(output,"        </Attribute>\n");
 fprintf(output,"        <Attribute AttributeType=\"Scalar\" Center=\"Edge\" Name=\"EdgeGroundwaterFlowRate\">\n"); 
-fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%i 3\" Format=\"HDF\" Precision=\"8\">state.nc:/%s/meshEdgeGroundwaterFlowRate</DataItem>\n",nele,grp_stt_name);
+fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%zu 3\" Format=\"HDF\" Precision=\"8\">state.nc:/%s/meshEdgeGroundwaterFlowRate</DataItem>\n",nele,grp_stt_name);
 fprintf(output,"        </Attribute>\n"); 
 fprintf(output,"        <Attribute AttributeType=\"Scalar\" Center=\"Edge\" Name=\"EdgeGroundwaterCumulativeFlow\">\n");
-fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%i 3\" Format=\"HDF\" Precision=\"8\">state.nc:/%s/meshEdgeGroundwaterCumulativeFlow</DataItem>\n",nele,grp_stt_name); 
+fprintf(output,"          <DataItem DataType=\"Float\" Dimensions=\"%zu 3\" Format=\"HDF\" Precision=\"8\">state.nc:/%s/meshEdgeGroundwaterCumulativeFlow</DataItem>\n",nele,grp_stt_name); 
 fprintf(output,"        </Attribute>\n");
 fprintf(output,"      </Grid>\n\n");
  
