aa4d091daef97eca23b24b17be0b55aa986066ed
Robert Steinke <rsteinke@uwyo.edu>
2014-10-02 13:09:12 -0600

    cleaned up preprocessing python scripts.
    Former-commit-id: 23acdcc4aef6d2c68c2013c5060d7d38474bd92e

---
+++ scripts/create_triangle_files.py
@@ -0,0 +1,186 @@
+# Run the following code on shapefiles that contain all of the catchment,
+# waterbody, and stream edges that you want to be in the simulation mesh.
+# These files must already have been pre-processed according to the mesh
+# creation instructions.  This code will generate the .poly and .node input
+# files for triangle and a .link file that contains the mapping from waterbody
+# permanent codes to link numbers.
+import math
+
+# modify these to point to your files
+input_directory_path       = "/user2/rsteinke/Desktop/tiny_green_mesh"
+input_catchment_file       = input_directory_path + "/" + "mesh_catchments.shp"
+input_waterbody_file       = input_directory_path + "/" + "mesh_waterbodies.shp"
+input_stream_file          = input_directory_path + "/" + "mesh_streams.shp"
+input_original_stream_file = input_directory_path + "/" + "projectednet.shp"
+output_directory_path      = "/user2/rsteinke/Desktop/tiny_green_mesh"
+output_node_file           = output_directory_path + "/" + "mesh.node"
+output_poly_file           = output_directory_path + "/" + "mesh.poly"
+output_link_file           = output_directory_path + "/" + "mesh.1.link"
+
+# To generate a point in a polygon generate a point on each side of the first
+# line segment in the polygon.  One of the points will be in the polygon.
+def point_in_polygon(feature):
+  geom = feature.geometry()
+  polygon = geom.asPolygon()
+  assert 0 < len(polygon) # No empty polygons.
+  x1 = polygon[0][0].x()
+  y1 = polygon[0][0].y()
+  x2 = polygon[0][1].x()
+  y2 = polygon[0][1].y()
+  mx = (x1 + x2) / 2
+  my = (y1 + y2) / 2
+  dx = (x1 - x2)
+  dy = (y1 - y2)
+  scale = 1 / (math.sqrt(dx * dx + dy * dy) * 1000)
+  testx = mx - dy * scale
+  testy = my + dx * scale
+  if not geom.contains(QgsPoint(testx, testy)):
+    testx = mx + dy * scale
+    testy = my - dx * scale
+    assert geom.contains(QgsPoint(testx, testy))
+  return testx, testy
+
+with open(output_node_file, "w") as node_file:
+  with open(output_poly_file, "w") as poly_file:
+    with open(output_link_file, "w") as link_file:
+      node    = 0 # Counter for the number of nodes.
+      segment = 0 # Counter for the number of segments.
+      region  = 0 # Counter for the number of regions.
+      hole    = 0 # Counter for the number of holes.
+      #
+      feature                  = QgsFeature()
+      catchment_layer          = QgsVectorLayer(input_catchment_file,       "catchment",       "ogr")
+      catchment_provider       = catchment_layer.dataProvider()
+      waterbody_layer          = QgsVectorLayer(input_waterbody_file,       "waterbody",       "ogr")
+      waterbody_provider       = waterbody_layer.dataProvider()
+      stream_layer             = QgsVectorLayer(input_stream_file,          "stream",          "ogr")
+      stream_provider          = stream_layer.dataProvider()
+      original_stream_layer    = QgsVectorLayer(input_original_stream_file, "original_stream", "ogr")
+      original_stream_provider = original_stream_layer.dataProvider()
+      #
+      node_file.write("0                    2 0 0\n") # Leave space for the first zero to be replaced with the number of nodes.
+      poly_file.write("0 2 0 0\n")                    # Zero nodes in the .poly file indicates nodes will be read from a separate .node file.
+      poly_file.write("0                    1\n")     # Leave space for the first zero to be replaced with the number of line segments.
+      #
+      # Determine which linknos are used in the original stream file.
+      used_linknos = []
+      linknoindex = original_stream_provider.fieldNameIndex("LINKNO")
+      assert -1 != linknoindex # LINKNO must be found.
+      original_stream_provider.select([linknoindex])
+      while original_stream_provider.nextFeature(feature):
+        linkno, success = feature.attributeMap()[linknoindex].toInt()
+        assert success # Integer conversion must succeed.
+        while linkno >= len(used_linknos):
+          used_linknos.append(-1)
+        used_linknos[linkno] = linkno
+      #
+      # Add the catchment nodes and segments to the mesh.
+      catchment_provider.select()
+      while catchment_provider.nextFeature(feature):
+        polygon = feature.geometry().asPolygon()
+        assert 0 < len(polygon) # No empty polygons.
+        boundary_marker = "0" # Catchment segments have no boundary marker.
+        for ring in polygon:
+          firstnode = node
+          for point in ring:
+            node_file.write(str(node) + " " + str(point.x()) + " " + str(point.y()) + "\n")
+            if node < firstnode + len(ring) - 1:
+              # Ordinary line segment from one node to the next.
+              poly_file.write(str(segment) + " " + str(node) + " " + str(node + 1)  + " " + boundary_marker + "\n")
+            else:
+              # Line segment from the last node in a closed polygon to its first node.
+              poly_file.write(str(segment) + " " + str(node) + " " + str(firstnode) + " " + boundary_marker + "\n")
+            node    += 1
+            segment += 1
+        region += 1
+      #
+      # Add the waterbody nodes and segments to the mesh.
+      permanentindex = waterbody_provider.fieldNameIndex("Permanent_")
+      assert -1 != permanentindex # Permanent_ must be found.
+      waterbody_provider.select([permanentindex])
+      while waterbody_provider.nextFeature(feature):
+        polygon = feature.geometry().asPolygon()
+        assert 0 < len(polygon) # No empty polygons.
+        permanent, success = feature.attributeMap()[permanentindex].toInt()
+        assert success
+        # FIXLATER this search could be sped up by storing a map of permanent -> linkno
+        linkno = 0
+        while linkno < len(used_linknos) and -1 != used_linknos[linkno] and permanent != used_linknos[linkno]:
+          linkno += 1
+        while linkno >= len(used_linknos):
+          used_linknos.append(-1)
+        used_linknos[linkno] = permanent
+        print "Waterbody number " + str(feature.id()) + " permanent code " + str(permanent) + " assigned to linkno " + str(linkno)
+        boundary_marker = str(-linkno) # The boundary marker indicates which waterbody.  Use negative because marker 1 is used by triangle.
+        for ring in polygon:
+          firstnode = node
+          for point in ring:
+            node_file.write(str(node) + " " + str(point.x()) + " " + str(point.y()) + "\n")
+            if node < firstnode + len(ring) - 1:
+              # Ordinary line segment from one node to the next.
+              poly_file.write(str(segment) + " " + str(node) + " " + str(node + 1)  + " " + boundary_marker + "\n")
+            else:
+              # Line segment from the last node in a closed polygon to its first node.
+              poly_file.write(str(segment) + " " + str(node) + " " + str(firstnode) + " " + boundary_marker + "\n")
+            node    += 1
+            segment += 1
+        hole += 1
+      #
+      # Add the stream nodes and segments to the mesh.
+      linknoindex = stream_provider.fieldNameIndex("LINKNO")
+      assert -1 != linknoindex # LINKNO must be found.
+      stream_provider.select([linknoindex])
+      while stream_provider.nextFeature(feature):
+        polyline = feature.geometry().asPolyline()
+        assert 0 < len(polyline) # No empty polylines.
+        linkno, success = feature.attributeMap()[linknoindex].toInt()
+        assert success # Integer conversion must succeed.
+        assert linkno == used_linknos[linkno]
+        boundary_marker = str(-linkno) # The boundary marker indicates which stream.  Use negative because marker 1 is used by triangle.
+        firstnode = node
+        for point in polyline:
+          node_file.write(str(node) + " " + str(point.x()) + " " + str(point.y()) + "\n")
+          if node < firstnode + len(polyline) - 1:
+            # Ordinary line segment from one node to the next.
+            poly_file.write(str(segment) + " " + str(node) + " " + str(node + 1) + " " + boundary_marker + "\n")
+            segment += 1
+          # No else.  No line segment from the last node in an open polyline to its first node.
+          node += 1
+      #
+      # Place a hole in each waterbody polygon.
+      # To do this we need to generate a point in each polygon.
+      poly_file.write(str(hole) + "\n")
+      hole = 0
+      waterbody_provider.select()
+      while waterbody_provider.nextFeature(feature):
+        holex, holey = point_in_polygon(feature)
+        poly_file.write(str(hole) + " " + str(holex) + " " + str(holey) + "\n")
+        hole += 1
+      #
+      # Place a region attribute and area constraint in each catchment polygon.
+      # To do this we need to generate a point in each polygon.
+      poly_file.write(str(region) + "\n")
+      region = 0
+      catchmentindex = catchment_provider.fieldNameIndex("Catchment")
+      assert -1 != catchmentindex # Catchment must be found.
+      catchment_provider.select([catchmentindex])
+      while catchment_provider.nextFeature(feature):
+        regionx, regiony = point_in_polygon(feature)
+        area_constraint = "-1" # FIXME calculate a real area constraint.
+        poly_file.write(str(region) + " " + str(regionx) + " " + str(regiony) + " " + feature.attributeMap()[catchmentindex].toString() + " " + area_constraint + "\n")
+        region += 1
+      #
+      # Fill in the number of segments and nodes at the beginning of the files.
+      node_file.seek(0)
+      node_file.write(str(node))
+      poly_file.seek(8)
+      poly_file.write(str(segment))
+      #
+      # Write out the mapping from waterbody permanent codes to link numbers.
+      link_file.write(str(len(used_linknos)) + " 1\n")
+      linkno = 0
+      while linkno < len(used_linknos):
+        link_file.write(str(linkno) + " " + str(used_linknos[linkno]) + "\n")
+        linkno += 1
+
+
--- scripts/create_z_file.py
+++ scripts/create_z_file.py
@@ -8,54 +8,35 @@ outputZFile   = "/share/CI-WATER Simulation Data/small_green_mesh/mesh.1.chan.z"
 
 with open(inputMeshFile, "r") as nodeFile:
   with open(outputZFile, "w") as zFile:
-    layer              = QgsRasterLayer(inputDEMFile)
-    firstLine          = True
-    firstNode          = True
-    numberOfNodes      = 0
-    nodeNumber         = 0
-    firstNodeNumber    = 0
-    previousNodeNumber = 0
+    layer          = QgsRasterLayer(inputDEMFile)
+    firstLine      = True
+    numberOfNodes  = 0
+    nodeNumber     = 0
+    nextNodeNumber = 0
+    #
     for line in nodeFile:
       if firstLine:
         # Process header line.
         firstLine = False
         match = re.match("\s*([-+]?\d+)", line)
-        if match:
-          numberOfNodes = int(match.group(1))
-          zFile.write(str(numberOfNodes) + " 1\n")
-        else:
-          print "ERROR: Invalid header."
-          break
+        assert match
+        numberOfNodes = int(match.group(1))
+        zFile.write(str(numberOfNodes) + " 1\n")
       else:
         # Process node line.
         match = re.match("\s*([-+]?\d+)\s*([-+]?(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?)\s*([-+]?(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?)", line)
-        if match:
-          nodeNumber = int(match.group(1))
-          if firstNode:
-            firstNode = False
-            if 0 == nodeNumber or 1 == nodeNumber:
-              firstNodeNumber    = nodeNumber
-              previousNodeNumber = nodeNumber - 1
-            else:
-              print "ERROR: Nodes must be numbered starting at zero or one."
-              break
-          if nodeNumber == previousNodeNumber + 1:
-            previousNodeNumber = nodeNumber
-            x = float(match.group(2))
-            y = float(match.group(6))
-            success, data = layer.identify(QgsPoint(x, y))
-            if success:
-              zFile.write(str(nodeNumber) + " " + str(data.values()[0]) + "\n")
-            else:
-              print "WARNING: Node " + nodeNumber + " is outside the extent of the DEM."
-              zFile.write(str(nodeNumber) + " 0.0\n")
-          else:
-            print "ERROR: Invalid node number " + nodeNumber + " should be " + (previousNodeNumber + 1)
-            break
+        assert match
+        nodeNumber = int(match.group(1))
+        assert nextNodeNumber == nodeNumber
+        nextNodeNumber += 1
+        x = float(match.group(2))
+        y = float(match.group(6))
+        success, data = layer.identify(QgsPoint(x, y))
+        if success:
+          zFile.write(str(nodeNumber) + " " + str(data.values()[0]) + "\n")
         else:
-          print "WARNING: No match found on line:"
-          print line
-    if nodeNumber != numberOfNodes - 1 + firstNodeNumber:
-      print "WARNING: Number of nodes in header does not match number of nodes in file."
+          print "WARNING: Node " + nodeNumber + " is outside the extent of the DEM.  Using zero for Z coordinate."
+          zFile.write(str(nodeNumber) + " 0.0\n")
+    assert nodeNumber == numberOfNodes - 1
 
 
