Fixed an off by one bug that was intruduced either by a change in the input files or a core change in Pandas.  Also updated the getMukey function to read a global prefix to find shape file paths.

scripts/readFromSSURGO_STATSGO_NLCD.py
@@ -33,8 +33,10 @@ STATSGO_AreaSymInd        = STATSGO_provider.fieldNameIndex("AREASYMBOL")
 STATSGO_MUKEYInd          = STATSGO_provider.fieldNameIndex("MUKEY")
 STATSGO_MUKEYSymInd       = STATSGO_provider.fieldNameIndex("MUSYM")
 
+#This script will read the SSURGO soil mukey files from this directory
+input_SSURGO_County_prefix = "/share/CI-WATER_Third_Party_Files/SSURGO/all/"
 # This script will read the NLCD data form this directory
-input_NLCD_directory_path = "/share/CI-WATER Third Party Files/NLCD/all_projected/"
+input_NLCD_directory_path = "/share/CI-WATER_Third_Party_Files/NLCD/all_projected/"
 
 # This script will read the mesh geometry from this directory
 # the files it will read are:
@@ -141,18 +143,20 @@ def getSoilTypDRV():
    #Since initially nodes are indexed based on thier ID, when they get aligned with the proper element here
    #Their index is off, so when we try to add these values into elements, they must have a corresponding index (1-num_elements)
    v = nodes.loc[elements['V1']].reset_index()
-   v.index = range(1, num_elems+1)
+   v.index = range(0, num_elems)
+   #Note v index and elements index must be the same!!!
    
    elements['X_center'] = v.X - 20000000.0
    elements['Y_center'] = v.Y - 10000000.0
    
+   
    v = nodes.loc[elements['V2']].reset_index()
-   v.index = range(1, num_elems+1)
+   v.index = range(0, num_elems)
    elements['X_center'] = elements['X_center'] + v.X - 20000000.0
    elements['Y_center'] = elements['Y_center'] + v.Y - 10000000.0
    
    v = nodes.loc[elements['V3']].reset_index()
-   v.index = range(1, num_elems+1)
+   v.index = range(0, num_elems)
    elements['X_center'] = elements['X_center'] + v.X - 20000000.0
    elements['Y_center'] = elements['Y_center'] + v.Y - 10000000.0
    
@@ -265,7 +269,7 @@ def getMUKEY(s):
 
             #TODO Maybe load all these into memory initally then lookup from dict???
             if AreaSym not in SSURGO_county_dict:
-                input_SSURGOperCounty_file = "/share/CI-WATER Third Party Files/SSURGO/all/soilmu_a_"+ AreaSym.toLower() + ".shp"
+                input_SSURGOperCounty_file = input_SSURGO_County_prefix+"soilmu_a_"+ AreaSym.toLower() + ".shp"
                 SSURGO_perCounty_layer  = QgsVectorLayer(input_SSURGOperCounty_file, "SSURGOperCounty",  "ogr")
                 SSURGO_perCounty_provider = SSURGO_perCounty_layer.dataProvider()
                 SSURGO_county_dict[AreaSym] = SSURGO_perCounty_layer
@@ -319,15 +323,15 @@ def getMUKEY(s):
                 break
 
     if not found:
+        #TODO/FIXME If not found, must handle this differently because the 
+        #int cast won't handle nan!!!
         MUKEY = np.nan
         AreaSym = np.nan
         inSSURGO = np.nan
-
     #Return the input series with MUKEY, AreaSym, and inSSURGO added to the series
     s['MUKEY'] = int(MUKEY)
     s['AreaSym'] = str(AreaSym)
     s['inSSURGO'] = inSSURGO
-    return s
 
 def getCOKEY(s):
 # in: