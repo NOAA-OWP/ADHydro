e59333013a355ea54bb44fe48d49089d8be7509b
Leticia Pureza <lpureza@yen.uwyo.edu>
2015-02-23 17:23:21 -0700

    Now the readFromSSURGO_STASTSGO_NLCD.py have the soil thickness values in meters instead of centimeters.

--- scripts/readFromSSURGO_STATSGO_NLCD.py
+++ scripts/readFromSSURGO_STATSGO_NLCD.py
@@ -573,6 +573,7 @@ def getSoilContent(s):
    co_key_rows = co_key_rows.apply(rel_percent_mod, axis=1)
    s['SoilType'] = co_key_rows.apply(getSoilTyp, axis = 1).values
    s['HorizonThickness'] = co_key_rows['hzdepb_r'].values
+   s['HorizonThickness'] = s['HorizonThickness']/100.0
    return s
 
 def getSoilTyp(s):
