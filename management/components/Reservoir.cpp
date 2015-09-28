35a3b432a7442735b92182a56796d107d6d8040b
nels j frazier <nfrazie1@grad05.cs.uwyo.edu>
2015-09-28 10:14:02 -0600

    Added comment in constructor

--- management/components/Reservoir.cpp
+++ management/components/Reservoir.cpp
@@ -4,6 +4,14 @@
 Reservoir::Reservoir(long long reachCode_):reachCode(reachCode_)
 {
     //Empty
+	/*
+	 * This is where connections to a database could be established and data
+	 * populated for the reservoirs.
+	 * In this object oriented model, perhaps a database loader object???
+	 *
+	 * This could also be pushed to the subclasses to call and load data.
+	 * Best to load whatever is standard across all reservoirs here, though.
+	 */
 }
 
 //Reservoir Destructor
