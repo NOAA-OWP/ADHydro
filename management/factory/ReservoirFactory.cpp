750b710e3e32116ea6963dd15231a5c636708ac8
nels j frazier <nfrazie1@grad05.cs.uwyo.edu>
2015-10-26 11:39:41 -0600

    Updated component location

--- management/factory/ReservoirFactory.cpp
+++ management/factory/ReservoirFactory.cpp
@@ -1,7 +1,7 @@
 #include "ReservoirCreator.h"
 //NEED TO INCLUDE EACH COMPONENT DEFINITION AS WELL AS ADD TO CONSTRUCTOR
-#include "../components/comp1.cpp"
-#include "../components/comp2.cpp"
+#include "../reservoir/comp1.cpp"
+#include "../reservoir/comp2.cpp"
 
 //ReservoirFactory Constructor
 ReservoirFactory::ReservoirFactory()
