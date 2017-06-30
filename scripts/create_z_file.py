15e536e1aa84f0eb095c60c028c1ebe19fe826fe
Nels John Frazier <hellkite500@gmail.com>
2017-06-30 13:40:39 -0600

    Added option to create channel files

--- scripts/create_z_file.py
+++ scripts/create_z_file.py
@@ -13,12 +13,19 @@ app.initQgis()
 parser = argparse.ArgumentParser(description="Create ADHydro z file from the results of triangle or adhydro_channel_preprocessing.")
 parser.add_argument('ADHydroMapDir', help='The ADHydro map directory containing sub-directories ASCII and TauDEM.')
 parser.add_argument('-o', '--outputDir', help='Optional output directory. By default, output will be put in ADHydroMapDir/ASCII')
+parser.add_argument('-b', '--base', help='Optional base name to add to file, i.e. <base>.tif. Defaults to projected/projected')
+parser.add_argument('-r', '--resolution', help='Stream resolutions to read (i.e. <base>_streams_20_meter.shp). By default looks for <base>_streams.shp.')
+parser.add_argument('-c', '--channel', help="Create z file for channel elements instead of mesh elements.  By default, operates on mesh.", action='store_true')
+
 args = parser.parse_args()
 
 if not os.path.isdir(args.ADHydroMapDir):
     parser.error("Directory '{}' does not exist.".format(args.ADHydroMapDir))
 
 meshPath = os.path.join(args.ADHydroMapDir, 'ASCII')
+if args.resolution:
+    meshPath = os.path.join(meshPath, '{}_meter'.format(args.resolution))
+
 demPath = os.path.join(args.ADHydroMapDir, 'TauDEM')
 
 if not os.path.isdir(meshPath):
@@ -27,23 +34,33 @@ if not os.path.isdir(demPath):
     parser.error("Directory '{}' does not exist.".format(netPath))
 
 inputMeshFile = os.path.join(meshPath, "mesh.1.node")
+if args.channel:
+    inputMeshFile = os.path.join(meshPath, "mesh.1.chan.node")
+
 if not os.path.isfile(inputMeshFile):
     parser.error("Cannot find file '{}'".format(inputMeshFile))
 
 taudemPath = os.path.join(args.ADHydroMapDir, 'TauDEM')
 if not os.path.isdir(taudemPath):
     parser.error("Cannot find TauDEM directory in '{}'".format(args.ADHydroMapDir))
-taudemPath = os.path.join(taudemPath, 'projected')
-#Check for either a single .tif or a .vrt
-inputDEMFile = os.path.join(taudemPath, 'projected.tif')
+
+if args.base:
+    inputDEMFile = os.path.join(taudemPath, '{}.tif'.format(args.base))
+else:
+    taudemPath = os.path.join(taudemPath, 'projected')
+    #Check for either a single .tif or a .vrt
+    inputDEMFile = os.path.join(taudemPath, 'projected.tif')
 if not os.path.isfile(inputDEMFile):
     print "No .tif found, looking for .vrt"
-    inputDEMFile = os.path.join(taudemPath, 'projected.vrt')
+    inputDEMFile[-3:] = '.vrt' # = os.path.join(taudemPath, 'projected.vrt')
     if not os.path.isfile(inputDEMFile):
         parser.error("Could not find appropriate DEM, neither projected.tif or projected.vrt exists in '{}'".format(taudemPath))
 print "Using '{}' for DEM".format(inputDEMFile)
 
 output_directory_path = os.path.join(args.ADHydroMapDir, 'ASCII')
+if args.resolution:
+    output_directory_path = os.path.join(output_directory_path, '{}_meter'.format(args.resolution))
+
 if args.outputDir:
     output_directory_path = args.outputDir
 if not os.path.isdir(output_directory_path):
@@ -51,7 +68,8 @@ if not os.path.isdir(output_directory_path):
     os.mkdir(output_directory_path)
 
 outputZFile   = os.path.join(output_directory_path, "mesh.1.z")
-
+if(args.channel):
+    outputZFile = os.path.join(output_directory_path, "mesh.1.chan.z")
 with open(inputMeshFile, "r") as nodeFile:
   with open(outputZFile, "w") as zFile:
     layer          = QgsRasterLayer(inputDEMFile)
