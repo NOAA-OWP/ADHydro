5cc57afb5941037a9782d6dbb511444d7be566c7
Robert Steinke <rsteinke@uwyo.edu>
2018-01-04 16:49:52 -0700

    incremental commit of big changes.

--- ADHydro_3.0/forcing_manager.cpp
+++ ADHydro_3.0/forcing_manager.cpp
@@ -1,3 +1,284 @@
 #include "forcing_manager.h"
 #include "adhydro.h"
 #include "forcing_manager.def.h"
+#include <netcdf.h>
+#include <netcdf_par.h>
+
+bool ForcingManager::checkInvariant() const
+{
+    bool error = false; // Error flag.
+    
+    if (!(NULL != jultime))
+    {
+        CkError("ERROR in ForcingManager::checkInvariant: jultime must not be NULL.\n");
+        error = true;
+    }
+    
+    if (!(jultimeIndex <= jultimeSize))
+    {
+        CkError("ERROR in ForcingManager::checkInvariant: jultimeIndex must be less than or equal to jultimeSize.\n");
+        error = true;
+    }
+    
+    if (jultimeIndex == jultimeSize && !(INFINITY == nextForcingTime))
+    {
+        CkError("ERROR in ForcingManager::checkInvariant: if jultimeIndex is equal to jultimeSize then nextForcingTime must be INFINITY.\n");
+        error = true;
+    }
+    
+    return error;
+}
+
+bool ForcingManager::readForcingTimes()
+{
+    bool   error    = false; // Error flag.
+    int    ncErrorCode;      // Return value of NetCDF functions.
+    int    fileID;           // ID of NetCDF file.
+    bool   fileOpen = false; // Whether fileID refers to an open file.
+    int    variableID;       // ID of variable in NetCDF file.
+    int    dimensionID;      // ID of dimension in NetCDF file.
+    bool   done     = false; // Flag to signal when we have found the right forcing index.
+    size_t newIndex;         // Possible new value for jultimeIndex.
+    double newForcingTime;   // (s) Possible new value for nextForcingTime.
+    
+    // Open the forcing file.
+    ncErrorCode = nc_open_par(Readonly::forcingFilePath.c_str(), NC_NETCDF4 | NC_MPIIO, MPI_COMM_WORLD, MPI_INFO_NULL, &fileID);
+    
+    if (NC_NOERR == ncErrorCode)
+    {
+        fileOpen = true;
+    }
+    else if (DEBUG_LEVEL & DEBUG_LEVEL_LIBRARY_ERRORS)
+    {
+        CkError("ERROR in ForcingManager::readForcingTimes: unable to open NetCDF forcing file %s.  NetCDF error message: %s.\n", Readonly::forcingFilePath.c_str(), nc_strerror(ncErrorCode));
+        error = true;
+    }
+    
+    // Get the dimension ID of the time dimension.
+    if (!error)
+    {
+        ncErrorCode = nc_inq_dimid(fileID, "Time", &dimensionID);
+        
+        if (DEBUG_LEVEL & DEBUG_LEVEL_LIBRARY_ERRORS)
+        {
+            if (!(NC_NOERR == ncErrorCode))
+            {
+                CkError("ERROR in ForcingManager::readForcingTimes: unable to get dimension Time in NetCDF forcing file.  NetCDF error message: %s.\n", nc_strerror(ncErrorCode));
+                error = true;
+            }
+        }
+    }
+    
+    // Get the dimension length of the time dimension.
+    if (!error)
+    {
+        ncErrorCode = nc_inq_dimlen(fileID, dimensionID, &jultimeSize);
+        
+        if (DEBUG_LEVEL & DEBUG_LEVEL_LIBRARY_ERRORS)
+        {
+            if (!(NC_NOERR == ncErrorCode))
+            {
+                CkError("ERROR in ForcingManager::readForcingTimes: unable to get length of dimension Time in NetCDF forcing file.  NetCDF error message: %s.\n", nc_strerror(ncErrorCode));
+                error = true;
+            }
+            else if ((0 < jultimeSize))
+            {
+                CkError("ERROR in ForcingManager::readForcingTimes: No forcing data in NetCDF forcing file.\n");
+                error = true;
+            }
+        }
+    }
+    
+    // Get the variable ID of the julian dates.
+    if (!error)
+    {
+        ncErrorCode = nc_inq_varid(fileID, "JULTIME", &variableID);
+        
+        if (DEBUG_LEVEL & DEBUG_LEVEL_LIBRARY_ERRORS)
+        {
+            if (!(NC_NOERR == ncErrorCode))
+            {
+                CkError("ERROR in ForcingManager::readForcingTimes: unable to get variable JULTIME in NetCDF forcing file.  NetCDF error message: %s.\n", nc_strerror(ncErrorCode));
+                error = true;
+            }
+        }
+    }
+    
+    // Get the julian date data.
+    if (!error)
+    {
+        jultime = new double[jultimeSize];
+        
+        ncErrorCode = nc_get_var(fileID, variableID, jultime);
+        
+        if (DEBUG_LEVEL & DEBUG_LEVEL_LIBRARY_ERRORS)
+        {
+            if (!(NC_NOERR == ncErrorCode))
+            {
+                CkError("ERROR in ForcingManager::readForcingTimes: unable to read variable JULTIME in NetCDF forcing file.  NetCDF error message: %s.\n", nc_strerror(ncErrorCode));
+                error = true;
+            }
+        }
+    }
+    
+    // Set jultimeIndex and nextForcingTime to the forcing data to use at the start of the simulation.
+    // We do a linear scan from the beginning of the array to find the last entry that is no later than Readonly::simulationStartTime.
+    // If we find entries that are not monotonically increasing in time we print a warning and those entries are ignored.
+    // It's possible that the first entry in the array is after Readonly::simulationStartTime.  In this case, we print a warning and use that entry.
+    if (!error)
+    {
+        jultimeIndex    = 0;
+        nextForcingTime = getForcingTime(jultimeIndex);
+        
+        while (!done)
+        {
+            // To protect against entries that are not monotonically increasing, find the next index strictly later than jultimeIndex when converted to a simulation time including roundoff to the nearest second.
+            newIndex = jultimeIndex;
+            
+            while (!(newForcingTime = getForcingTime(++newIndex) > nextForcingTime) && newIndex < jultimeSize)
+            {
+                if (2 <= Readonly::verbosityLevel)
+                {
+                    CkError("WARNING in ForcingManager::readForcingTimes: forcing file entry %lu is not monotonically increasing in time when rounded to the nearest second.\n", newIndex);
+                }
+            }
+            
+            if (!(newForcingTime <= Readonly::simulationStartTime) || newIndex == jultimeSize)
+            {
+                // If we found one later than Readonly::simulationStartTime or hit the end of the array then we are done, and we will use the entry previously stored in jultimeIndex.
+                done = true;
+            }
+            else
+            {
+                // Otherwise, the new one is later than jultimeIndex and no later than Readonly::simulationStartTime so save it and keep scanning.
+                jultimeIndex    = newIndex;
+                nextForcingTime = newForcingTime;
+            }
+        }
+        
+        // Print a warning if we didn't find an entry no later than Readonly::simulationStartTime.
+        // In that case, the first entry will still be used.
+        if (2 <= Readonly::verbosityLevel && !(nextForcingTime <= Readonly::simulationStartTime))
+        {
+            CkError("WARNING in ForcingManager::readForcingTimes: first entry in forcing file is after simulation start time.\n");
+        }
+    }
+    
+    // Close the forcing file.
+    if (fileOpen)
+    {
+        ncErrorCode = nc_close(fileID);
+        
+        if (DEBUG_LEVEL & DEBUG_LEVEL_LIBRARY_ERRORS)
+        {
+            if (!(NC_NOERR == ncErrorCode))
+            {
+                CkError("ERROR in ForcingManager::readForcingTimes: could not close NetCDF forcing file %s.  NetCDF error message: %s.\n", Readonly::forcingFilePath.c_str(), nc_strerror(ncErrorCode));
+                error = true;
+            }
+        }
+    }
+    
+    return error;
+}
+
+bool ForcingManager::readAndSendForcing()
+{
+    bool   error    = false; // Error flag.
+    int    ncErrorCode;      // Return value of NetCDF functions.
+    int    fileID;           // ID of NetCDF file.
+    bool   fileOpen = false; // Whether fileID refers to an open file.
+    int    variableID;       // ID of variable in NetCDF file.
+    long   year;             // For printing date and time of forcing data.
+    long   month;            // For printing date and time of forcing data.
+    long   day;              // For printing date and time of forcing data.
+    long   hour;             // For printing date and time of forcing data.
+    long   minute;           // For printing date and time of forcing data.
+    double second;           // For printing date and time of forcing data.
+    size_t newIndex;         // Possible new value for jultimeIndex.
+    double newForcingTime;   // (s) Possible new value for nextForcingTime.
+    
+    if (DEBUG_LEVEL & DEBUG_LEVEL_PUBLIC_FUNCTIONS_SIMPLE)
+    {
+        if (!(jultimeIndex < jultimeSize))
+        {
+            CkError("ERROR in ForcingManager::readAndSendForcing: someone just asked for forcing data beyond the end of the array, which is an error.\n");
+            error = true;
+        }
+    }
+    
+    if (!error)
+    {
+        // Print out that we are sending the forcing.
+        if (0 == CkMyPe() && 1 <= Readonly::verbosityLevel)
+        {
+            julianToGregorian(jultime[jultimeIndex], &year, &month, &day, &hour, &minute, &second);
+            CkPrintf("Reading forcing data for %02ld/%02ld/%04ld %02ld:%02ld:%02.0lf\n", month, day, year, hour, minute, second);
+        }
+        
+        // Open the forcing file.
+        ncErrorCode = nc_open_par(Readonly::forcingFilePath.c_str(), NC_NETCDF4 | NC_MPIIO, MPI_COMM_WORLD, MPI_INFO_NULL, &fileID);
+        
+        if (NC_NOERR == ncErrorCode)
+        {
+            fileOpen = true;
+        }
+        else if (DEBUG_LEVEL & DEBUG_LEVEL_LIBRARY_ERRORS)
+        {
+            CkError("ERROR in ForcingManager::readAndSendForcing: unable to open NetCDF forcing file %s.  NetCDF error message: %s.\n", Readonly::forcingFilePath.c_str(), nc_strerror(ncErrorCode));
+            error = true;
+        }
+    }
+    
+    if (!error)
+    {
+        // Load the forcing.
+        // FIXME implement
+        
+        // Increment jultimeIndex and update nextForcingTime.
+        // To protect against entries that are not monotonically increasing, find the next index strictly later than jultimeIndex when converted to a simulation time including roundoff to the nearest second.
+        newIndex = jultimeIndex;
+        
+        while (!(newForcingTime = getForcingTime(++newIndex) > nextForcingTime) && newIndex < jultimeSize)
+        {
+            if (2 <= Readonly::verbosityLevel)
+            {
+                CkError("WARNING in ForcingManager::readAndSendForcing: forcing file entry %lu is not monotonically increasing in time when rounded to the nearest second.\n", newIndex);
+            }
+        }
+        
+        jultimeIndex    = newIndex;
+        nextForcingTime = newForcingTime;
+        
+        // Print a warning if we are sending the last forcing time in the file.
+        if (2 <= Readonly::verbosityLevel && jultimeIndex == jultimeSize)
+        {
+            CkError("WARNING in ForcingManager::readAndSendForcing: reading the last entry in the forcing file.  No more forcing will be loaded in the future for this run.\n");
+        }
+        
+        // Send the forcing and nextForcingTime.
+        // FIXME implement
+        //for (ii = Readonly::localRegionStart; ii < Readonly::localRegionStart + Readonly::localNumberOfRegions; ++ii)
+        //{
+        //    // FIXME send forcing data to regions.
+        //    ADHydro::regionProxy[ii].sendForcing(nextForcingTime);
+        //}
+    }
+    
+    // Close the forcing file.
+    if (fileOpen)
+    {
+        ncErrorCode = nc_close(fileID);
+        
+        if (DEBUG_LEVEL & DEBUG_LEVEL_LIBRARY_ERRORS)
+        {
+            if (!(NC_NOERR == ncErrorCode))
+            {
+                CkError("ERROR in ForcingManager::readAndSendForcing: could not close NetCDF forcing file %s.  NetCDF error message: %s.\n", Readonly::forcingFilePath.c_str(), nc_strerror(ncErrorCode));
+                error = true;
+            }
+        }
+    }
+    
+    return error;
+}
--- ADHydro_3.0/forcing_manager.h
+++ ADHydro_3.0/forcing_manager.h
@@ -5,7 +5,7 @@
 #include "all.h"
 #include "forcing_manager.decl.h"
 
-// ForcingManager is a Charm++ group that reads forcing data from file and sends it to the appropriate regions.
+// ForcingManager is a Charm++ group that reads forcing data from file and sends it to the elements.
 class ForcingManager : public CBase_ForcingManager
 {
     ForcingManager_SDAG_CODE
@@ -13,19 +13,84 @@ class ForcingManager : public CBase_ForcingManager
 public:
     
     // Constructor.
-    inline ForcingManager() : nextForcingTime(Readonly::simulationStartTime)
+    inline ForcingManager() : jultime(NULL), jultimeSize(0), jultimeIndex(0), nextForcingTime(0.0)
     {
-        // Tell myself I'm ready for the first forcing data.  After the first one, Regions will tell me when they are ready for the next one.
+        if (readForcingTimes())
+        {
+            CkExit();
+        }
+        
+        // We have to call readForcingTimes to initialize member variables before we will pass the invariant.
+        if (DEBUG_LEVEL & DEBUG_LEVEL_PUBLIC_FUNCTIONS_SIMPLE)
+        {
+            if (checkInvariant())
+            {
+                CkExit();
+            }
+        }
+        
+        // Tell myself I'm ready to send the first forcing data.  After the first one, Regions will tell me when they are ready for the next one.
         thisProxy[CkMyPe()].readyForForcing(nextForcingTime);
         thisProxy[CkMyPe()].runUntilSimulationEnd();
     }
     
-    // FIXME checkInvariant?
+    // Destructor.
+    inline ~ForcingManager()
+    {
+        delete[] jultime;
+    }
+    
+    // Check invariant conditions on data.
+    //
+    // Returns: true if the invariant is violated, false otherwise.
+    bool checkInvariant() const;
     
 private:
     
-    // Simulation time.
-    double       nextForcingTime; // (s) The next simulation time when new forcing data needs to be read.
+    // Initialize member variables to the correct forcing instance for the start of the simulation.
+    //
+    // Returns: true if there is an error, false otherwise.
+    bool readForcingTimes();
+    
+    // Read from the forcing file the instance at jultimeIndex.  Then update jultimeIndex and nextForcingTime to the next instance in the forcing file.
+    // When advancing to the next instance protect against jultime being not monotonically increasing and against running off the end of the array.
+    // Then send the forcing data and nextForcingTime out to the appropriate elements.
+    //
+    // Returns: true if there is an error, false otherwise.
+    bool readAndSendForcing();
+    
+    // Returns: (s) the simulation time of jultime[index] or INFINITY if index is off the end of the array.
+    //          We round this value to the nearest second because we have had problems with roundoff error setting forcing times to 59.999999... seconds.
+    //
+    // Parameters:
+    //
+    // index - The entry of jultime to get as a simulation time.
+    inline double getForcingTime(size_t index)
+    {
+        double newTime; // Return value.
+        
+        if (DEBUG_LEVEL & DEBUG_LEVEL_PRIVATE_FUNCTIONS_SIMPLE)
+        {
+            CkAssert(index <= jultimeSize);
+        }
+        
+        if (index >= jultimeSize)
+        {
+            newTime = INFINITY;
+        }
+        else
+        {
+            newTime = round((jultime[index] - Readonly::referenceDate) * ONE_DAY_IN_SECONDS);
+        }
+        
+        return newTime;
+    }
+    
+    // Simulation time of forcing instances.
+    double*      jultime;         // Array of Julian dates of all instances in forcing file.
+    size_t       jultimeSize;     // The size of the allocated array pointed to by jultime.
+    size_t       jultimeIndex;    // The next instance to use in the forcing file.
+    double       nextForcingTime; // (s) The simulation time of the instance at jultimeIndex or INFINITY if jultimeIndex is off the end of the array.
     const double simulationEndTime = Readonly::simulationStartTime + Readonly::simulationDuration;
                                   // This is partly for efficiency so we don't do the addition over and over and partly because Charm++ is having trouble parsing Readonly:: in the .ci file.
 };
